9a203187aa5bcdecfa240131f207c989730b3194
Germán Lugo <german.lugo@bitnibs.com>
2018-08-23 01:25:01 +0200

    Purge unneeded variables.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,3 @@
-var AUtils = artifacts.require("./AddressUtils.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
-var ERC721Receiver = artifacts.require("./ERC721Receiver.sol");
-var ERC165 = artifacts.require("./ERC165.sol");
-var ERC165Basic = artifacts.require("./ERC721Basic.sol");
-var ERC721 = artifacts.require("./ERC721.sol");
-var SupportsInterfaceWithLookup = artifacts.require("./SupportsInterfaceWithLookup.sol");
-var ERC721BasicToken = artifacts.require("./ERC721BasicToken.sol");
 var ERC721Token = artifacts.require("./ERC721Token.sol");
 
 module.exports = function(deployer) {
