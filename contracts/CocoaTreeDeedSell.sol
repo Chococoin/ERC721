a61252ac622e554dd3ef6b1dfa52c776781bb647
Germán Lugo <german.lugo@bitnibs.com>
2018-08-27 21:29:54 +0200

    WIP.

--- contracts/CocoaTreeDeedSell.sol
+++ contracts/CocoaTreeDeedSell.sol
@@ -5,27 +5,48 @@ import './SafeMath.sol';
 
 contract CocoaTreeDeedSell is SafeMath{
   CocoaVirtualField public deedStore;
-  address admin;
+  address public admin;
   uint256 public tokenPrice;
-  uint256 public tokensSold;
+  uint256 public treesSold;
+  uint256 public deedStock;
   bool public paused;
 
   modifier notPaused{
-    require(notPaused == false);
+    require(paused == false);
     _; 
   }
 
+  modifier onlyAdmin{
+    require(msg.sender == admin);
+    _;
+  }
+
   event sell(address _buyer, uint256 _amount);
+  event contractState(bool paused);
 
   constructor(CocoaVirtualField _deedStore, uint256 _tokePrice) public {
     admin = msg.sender;
-    tokenContract = _deedStore;
+    deedStore = _deedStore;
     tokenPrice = _tokePrice;
     paused = false;
   }
 
-  function buyDeeds(uint256 _numberOfTokens) notPaused payable return(bool){
-    require(msg.value == mul(_numberOfTokens, tokenPrice));
-    
+  function treesInStock() external view returns(uint256){
+    return deedStore.balanceOf(this);
   }
+
+  function buyDeeds(uint256 _numberOfTokens) external notPaused payable returns(bool){
+    require(msg.value == mul(_numberOfTokens, tokenPrice));
+    require(deedStore.balanceOf(this) >= _numberOfTokens);
+    require(deedStore.transfer(_numberOfTokens));
+    treesSold += _numberOfTokens;
+    emit sell(msg.sender, _numberOfTokens);
+    return true;
+  }
+
+  function pauseContract() external notPaused onlyAdmin returns (bool){
+    emit contractState(bool);
+    return (paused = true);
+  }
+
 }
